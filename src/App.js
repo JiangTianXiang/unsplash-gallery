29218978d55587e695c2db78759b463eb5e14f82
JiangTianXiang <jasonjiang1999@gmail.com>
2021-06-11 09:41:47 -0400

    Implemented basic toggle for dark back ground

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Home from "pages/Home";
 import Explore from "pages/Explore";
 import NavBar from "components/NavBar";
-import User from "pages/User"
+import User from "pages/User";
 import SearchPhotoResult from "pages/SearchPhotoResult";
 import SearchCollectionResult from "pages/SearchCollectionResult";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Poppins";
+    text-align: center;
+  }
+`;
+
+const theme = {
+  main: "#000000",
+  secondary: "#FFFFFF",
+};
+
 export default function App() {
   return (
-    <Router>
-      <NavBar />
-      {/* A <Switch> looks through its children <Route>s and
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <NavBar />
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/user/:name" component={User}></Route>
-        <Route path="/search/photos/:input" component={SearchPhotoResult}></Route>
-        <Route path="/search/collections/:input" component={SearchCollectionResult}></Route>
-        <Route path="/explore" component={Explore}></Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/user/:name" component={User}></Route>
+          <Route
+            path="/search/photos/:input"
+            component={SearchPhotoResult}
+          ></Route>
+          <Route
+            path="/search/collections/:input"
+            component={SearchCollectionResult}
+          ></Route>
+          <Route path="/explore" component={Explore}></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { NavBarContianer } from "./NavBar.styles";
 import Search from "components/Search";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "Poppins";
+    text-align: center;
+    background-color: ${(props) => props.theme.secondary};
+    color: ${(props) => props.theme.main}
+  }
+`;
+
+const lightTheme = {
+  main: "#000000",
+  secondary: "#FFFFFF",
+};
+
+const darkTheme = {
+  secondary: "#000000",
+  main: "#FFFFFF",
+};
+
 class NavBar extends React.Component {
+  state = {
+    on: true,
+  };
+
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };
+
   render() {
     return (
-      <NavBarContianer>
-        <Link to="/">Home</Link>
-        <Search />
-        <Link to="/explore">Explore</Link>
-      </NavBarContianer>
+      <ThemeProvider theme={this.state.on ? lightTheme : darkTheme}>
+        <GlobalStyle />
+        <NavBarContianer>
+          <Link to="/">Home</Link>
+          <Search />
+          <Link to="/explore">Explore</Link>
+          <button onClick={this.handleClick}>{this.state.on ? "Light" : "Dark"}</button>
+        </NavBarContianer>
+      </ThemeProvider>
     );
   }
 }
