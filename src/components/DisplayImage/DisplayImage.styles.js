176c84c9c042a3bed9c1734d10ac1bf5c6859325
JiangTianXiang <jasonjiang1999@gmail.com>
2021-07-10 10:56:10 -0400

    Adjust home page image jiggle

--- src/components/DisplayImage/DisplayImage.styles.js
+++ src/components/DisplayImage/DisplayImage.styles.js
@@ -13,7 +13,7 @@ export const Overlay = styled.div`
   height: 100%;
   opacity: ${(props) => props.opacity};
   background-color: ${(props) => `${props.placeholderColor || "grey"}`};
-  max-width: 830px;
+  min-width: 830px;
   border-radius: 8px;
   transition: 0.5s;
 `;
